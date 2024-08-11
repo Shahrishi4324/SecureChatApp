5e3873f3c92fa52214ba42d2720cbc12d04eb9a5
Rishi Shah <smitjohn3221@gmail.com>
2024-08-11 16:24:20 -0400

    Add files via upload

--- chat_client.py
+++ chat_client.py
@@ -1,11 +1,23 @@
 import socket
+import threading
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if message:
+                print(f"\nServer: {message}")
+            else:
+                break
+        except:
+            break
 
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 9999))
     
-    welcome_message = client_socket.recv(1024).decode('utf-8')
-    print(welcome_message)
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.start()
     
     while True:
         message = input("You: ")
--- chat_server.py
+++ chat_server.py
@@ -1,4 +1,16 @@
 import socket
+import threading
+
+def handle_client(client_socket, client_address):
+    print(f"Client {client_address} connected.")
+    client_socket.send("Welcome to the chat!".encode('utf-8'))
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        if message:
+            print(f"Client {client_address}: {message}")
+        else:
+            break
+    client_socket.close()
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,15 +20,8 @@ def start_server():
 
     while True:
         client_socket, client_address = server_socket.accept()
-        print(f"Client {client_address} connected.")
-        client_socket.send("Welcome to the chat!".encode('utf-8'))
-        while True:
-            message = client_socket.recv(1024).decode('utf-8')
-            if message:
-                print(f"Client {client_address}: {message}")
-            else:
-                break
-        client_socket.close()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
 
 if __name__ == "__main__":
     start_server()
